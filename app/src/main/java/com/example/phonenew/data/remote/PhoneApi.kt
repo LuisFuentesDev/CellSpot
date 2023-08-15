b3627b37f6ded1a272a05dadf7e63014709d944c
LuisFuentesDev <lsfuentes2@gmail.com>
2023-08-15 14:03:15 -0400

    Se termina el adapter

--- app/src/main/java/com/example/phonenew/data/remote/PhoneApi.kt
+++ app/src/main/java/com/example/phonenew/data/remote/PhoneApi.kt
@@ -4,6 +4,6 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface PhoneApi {
-    @GET("products/")
+    @GET("products")
     suspend fun getData(): Response<List<DataPhone>>
 }
---
+++ app/src/main/java/com/example/phonenew/vista/AdapterPhone.kt
@@ -0,0 +1,63 @@
+package com.example.phonenew.vista
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.phonenew.R
+import com.example.phonenew.data.local.PhoneEntity
+import com.example.phonenew.databinding.ItemPhoneBinding
+
+class AdapterPhone : RecyclerView.Adapter<AdapterPhone.ItemPhoneViewHolder>() {
+
+    lateinit var binding: ItemPhoneBinding
+    private val listItemPhones = mutableListOf<PhoneEntity>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+
+    ): AdapterPhone.ItemPhoneViewHolder {
+
+        binding = ItemPhoneBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemPhoneViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AdapterPhone.ItemPhoneViewHolder, position: Int) {
+
+        val phone = listItemPhones[position]
+        holder.bind(phone)
+    }
+
+    fun setData(phones: List<PhoneEntity>) {
+        this.listItemPhones.clear()
+        this.listItemPhones.addAll(phones)
+        notifyDataSetChanged()
+
+    }
+
+    override fun getItemCount(): Int {
+        return listItemPhones.size
+    }
+
+    class ItemPhoneViewHolder(val phoneVista: ItemPhoneBinding) :
+        RecyclerView.ViewHolder(phoneVista.root) {
+
+        fun bind(phone: PhoneEntity) {
+            phoneVista.imageViewPhone.load(phone.image)
+            phoneVista.textViewName.text = phone.name
+            phoneVista.textViewPrice.text = phone.price.toString()
+
+            phoneVista.cardViewPhone.setOnClickListener {
+                val bundle = Bundle()
+                bundle.putString("id", phone.id)
+                Navigation.findNavController(phoneVista.root)
+                    .navigate(R.id.action_phoneList_to_phoneDetails, bundle)
+
+            }
+
+        }
+    }
+}
