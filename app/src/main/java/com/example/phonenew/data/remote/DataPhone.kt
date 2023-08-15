1eccc9ec3e26f5099f8031ac9b4f471e50eb2b8f
LuisFuentesDev <lsfuentes2@gmail.com>
2023-08-15 12:05:15 -0400

    Se crea el remote

---
+++ app/src/main/java/com/example/phonenew/data/remote/DataPhone.kt
@@ -0,0 +1,4 @@
+package com.example.phonenew.data.remote
+
+data class DataPhone(val id:String, val name:String,val price:Int, val image:String)
+
---
+++ app/src/main/java/com/example/phonenew/data/remote/PhoneApi.kt
@@ -0,0 +1,9 @@
+package com.example.phonenew.data.remote
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PhoneApi {
+    @GET("products")
+    suspend fun getData(): Response<DataPhone>
+}
